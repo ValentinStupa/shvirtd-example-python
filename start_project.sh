02f0b4e806753984606cbda88f2fb438ee1cc97b
valentin stupa <valentin.stupa@ediweb.com>
2024-05-08 18:03:01 +0300

    update script

--- start_project.sh
+++ start_project.sh
@@ -2,6 +2,18 @@
 #
 #           !!! Run with 'sudo' privilage under your user
 #
+#---------------------------
+#   Install docker at least
+#---------------------------
+#   sudo yum install git vim telnet
+#   sudo yum install -y yum-utils
+#   sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+#   sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#   sudo systemctl start docker
+#   docker -v
+#   docker compose version
+
+
 GIT_PROJECT=https://github.com/ValentinStupa/shvirtd-example-python.git
 DEST_DIR=/opt
 PROJECT_NAME=`basename $GIT_PROJECT | sed -e 's=.git==g'`
